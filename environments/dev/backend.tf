4799c42c447b8b662a2bab6fa701ecda4995968b
mohamedhassan81 <mohamed_hassan@shi.com>
2021-08-04 10:52:50 +0000

    updates project id and buckets

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "asg-cloud-delivery-gcp-tfstate"
     prefix = "env/dev"
   }
 }
--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="asg-cloud-delivery-gcp"
